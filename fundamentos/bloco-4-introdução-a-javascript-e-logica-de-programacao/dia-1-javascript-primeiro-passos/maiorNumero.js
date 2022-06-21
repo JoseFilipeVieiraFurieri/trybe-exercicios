37667204bfe911cd6c3c55da64af2b298a00e890
José Filipe Vieira Furieri <jf.furieri@ymail.com>
2022-06-21 20:01:54 -0300

    resolvendo topicos 1 e 2

---
+++ fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiro-passos/maiorNumero.js
@@ -0,0 +1,9 @@
+const numero1=20;
+
+const numero2=30;
+
+if (numero1 > numero2) {
+    console.log (numero1+" "+"é maior")
+} else { 
+    console.log (numero2+" "+"é maior")
+}
---
+++ fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiro-passos/script.js
@@ -0,0 +1,9 @@
+const a=10;
+const b=4;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
