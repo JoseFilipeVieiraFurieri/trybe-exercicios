93de6b1c5b1a8e8df76b010ae9cc337be1bdf5ef
José Filipe Vieira Furieri <jf.furieri@ymail.com>
2022-06-26 14:17:45 -0300

    parte 1 - exercicios 2, 3, 4.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-objectos-e-funçoes/Parte1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-objectos-e-funçoes/Parte1.js
@@ -1,3 +1,5 @@
+// Exercicio 1
+
 // let info = {
 //     personagem: 'Margarida',
 //     origem: 'Pato Donald',
@@ -5,4 +7,59 @@
 //   };
 
 //   console.log('Bem-vinda, ' + info.personagem)
+
+// exercicio 2
+
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   };
+
+//   info['Recorrente'] = 'Sim';
+
+//   console.log(info);
+
+// Exercicio 3
+
+// let info = {
+//         personagem: 'Margarida',
+//         origem: 'Pato Donald',
+//         nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//       };
+    
+//       info['Recorrente'] = 'Sim';
+
+
+//       for (let chave in info) {
+//         console.log(chave);
+//       }
+
+// Exercicio 4
+
+let info = {
+        personagem: 'Margarida',
+        origem: 'Pato Donald',
+        nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+      };
+    
+      info['Recorrente'] = 'Sim';
+
+
+      for (let chave in info) {
+        console.log(info[chave]);
+      }
+
+
+
+
+
+
+
+
+
+
+  
+
+ 
 
